ccee923b59ed5ea1b5f466955cd7848b926a17a5
Fer Bueno <ferbuenob@gmail.com>
2020-08-13 00:59:12 -0500

    Added Testimonies as a valid page

---
+++ src/pages/Testimonies/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Row, Carousel } from 'antd';
+import { Floor, SEO } from '@/components';
+
+export default () => {
+  <Floor id="testimonies">
+    <SEO title="Testimonies"/>
+    Testimonies
+  </Floor>
+}
